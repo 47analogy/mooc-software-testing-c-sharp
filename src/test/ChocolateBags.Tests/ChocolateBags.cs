4188e1b0a66c3c2fc1300fab2d7b5607d30b8628
analogy47 <actweed47@gmail.com>
2021-11-23 23:38:06 -0500

    Add ChocolateBags Parameterized Boundary Tests

--- src/test/ChocolateBags.Tests/ChocolateBags.cs
+++ src/test/ChocolateBags.Tests/ChocolateBags.cs
@@ -5,40 +5,60 @@ namespace ChocolateBags.Tests
 {
   public class ChocolateBagsTests
   {
-    [Fact]
+    [Theory]
+    [InlineData(1, 1, 5, 0)]
+    [InlineData(1, 1, 6, 1)]
+    [InlineData(1, 1, 7, -1)]
+    [InlineData(1, 1, 8, -1)]
     //total is higher than the amount of small and big bars (i.e. sm = 1, big = 1, tot = 10)
-    public void totalIsBiggerThanAmountOfBars()
+    public void totalIsBiggerThanAmountOfBars(int small, int big, int total, int expected)
     {
       ChocolateBags cb = new ChocolateBags();
-      int result = cb.calculate(1, 1, 10);
-      Assert.Equal(-1, result);
+      int result = cb.calculate(small, big, total);
+      Assert.Equal(expected, result);
     }
 
-    [Fact]
+    [Theory]
+    [InlineData(4, 0, 10, -1)]
+    [InlineData(4, 1, 10, -1)]
+    [InlineData(5, 2, 10, 0)]
+    [InlineData(5, 3, 10, 0)]
     // only big bars (i.e. sm = 5, big = 3, tot = 10) -> return 0
-    public void onlyBigBars()
+    public void onlyBigBars(int small, int big, int total, int expected)
     {
       ChocolateBags cb = new ChocolateBags();
-      int result = cb.calculate(5, 3, 10);
-      Assert.Equal(0, result);
+      int result = cb.calculate(small, big, total);
+      Assert.Equal(expected, result);
     }
 
-    [Fact]
+    [Theory]
+    [InlineData(0, 3, 17, -1)]
+    [InlineData(1, 3, 17, -1)]
+    [InlineData(2, 3, 17, 2)]
+    [InlineData(3, 3, 17, 2)]
+    [InlineData(0, 3, 12, -1)]
+    [InlineData(1, 3, 12, -1)]
+    [InlineData(2, 3, 12, 2)]
+    [InlineData(3, 3, 12, 2)]
     // big and small bars (i.e. sm = 5, big = 3, tot = 17) -> return 2
-    public void bigAndSmallBars()
+    public void bigAndSmallBars(int small, int big, int total, int expected)
     {
       ChocolateBags cb = new ChocolateBags();
-      int result = cb.calculate(5, 3, 17);
-      Assert.Equal(2, result);
+      int result = cb.calculate(small, big, total);
+      Assert.Equal(expected, result);
     }
 
-    [Fact]
+    [Theory]
+    [InlineData(4, 2, 3, 3)]
+    [InlineData(3, 2, 3, 3)]
+    [InlineData(2, 2, 3, -1)]
+    [InlineData(1, 2, 3, -1)]
     // only small bars (ie. sm = 4, big = 2, total = 3) -> returns 3
-    public void onlySmallBars()
+    public void onlySmallBars(int small, int big, int total, int expected)
     {
       ChocolateBags cb = new ChocolateBags();
-      int result = cb.calculate(4, 2, 3);
-      Assert.Equal(3, result);
+      int result = cb.calculate(small, big, total);
+      Assert.Equal(expected, result);
     }
   }
 }
